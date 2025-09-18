f1fa144e11595388a7a2c8f7c29e9473ecd3a93e
chan60 <geunchan@ninanobiz.com>
2025-09-18 11:38:48 +0900

    d

--- examples/python/2-5_command_to_motor.py
+++ examples/python/2-5_command_to_motor.py
@@ -269,7 +269,7 @@ def compare_attitudes():
             )
             # g.command_motor("roll", round(float(sitl.roll_angle_radians - fc.roll_angle_radians), 2))
             # g.command_motor("pitch", round(float(sitl.pitch_angle_radians - fc.pitch_angle_radians), 2))
-            g.command_motor("yaw", roll_diff)
+            g.command_motor("yaw", yaw_diff)
         else:
             print(
                 f" Roll: {sitl.roll_angle}, Pitch: {sitl.pitch_angle}, Yaw: {sitl.yaw_angle}, FC Roll: {fc.roll_angle}, Pitch: {fc.pitch_angle}, Yaw: {fc.yaw_angle}"
